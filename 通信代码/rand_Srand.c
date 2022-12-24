be4d9d210af4d39b983c09373596809d34b1417d
sysu-desktop <2716705056@qq.com>
2022-12-24 17:40:17 +0800

    just update

---
+++ 通信代码/rand_Srand.c
@@ -0,0 +1,32 @@
+/*************************************************************************
+>> File Name: rand_Srand.c
+>> Author: 陈俊杰
+>> Mail: 2716705056qq.com
+
+>> Created Time: 2022年11月05日 星期六 20时03分47秒
+
+>> 此程序的功能是：
+************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<float.h>
+#include<limits.h>
+#include<math.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<stddef.h>
+#include<locale.h>
+#include<time.h>
+#include<complex.h>
+#include<stdbool.h>
+
+
+
+
+int main(int argc, char *argv[])
+{
+    int a = rand();
+    printf("%d\n",a);
+    return 0;
+}
